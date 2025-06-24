13535c49b394d5231a80fb5aebb34238ebb7721f
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-06-24 08:53:49 -0700

    creation of dashoard

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask , render_template , request,  redirect
+from flask import Flask , render_template , request,  redirect, session , url_for
 
 app = Flask(__name__)
 
 
 
 @app.route("/")
-def index():
+def home():
+    if"username" in session :
+        return redirect(url_for('dashboard'))
     return render_template("index.html")
 
 
